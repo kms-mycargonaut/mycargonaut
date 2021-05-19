a887b8381813468e3413dbc72c0d6de6602b83a8
Patricia <patricia.ehrenreich@mni.thm.de>
2021-05-19 12:50:02 +0200

    Added user class with attributes.

---
+++ src/app/model/user.ts
@@ -0,0 +1,18 @@
+export class User {
+  id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  birthday?: string;
+  image?: string;
+  constructor(id: string, email: string, password: string, firstName: string, lastName: string, birthday?: string, image?: string) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = birthday;
+    this.image = image;
+  }
+}
