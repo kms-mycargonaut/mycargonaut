7e03c54ed859a0524162bcbdf3c8d272b4020251
Bastian Jakobi <>
2021-05-25 17:02:33 +0200

    Fixed #elseBlock issue in 'search-page'

--- src/app/search-page/search-page.component.spec.ts
+++ src/app/search-page/search-page.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchPageComponent } from './search-page.component';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from '../../environments/environment';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import {RouterTestingModule} from '@angular/router/testing';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SearchPageComponent', () => {
   let component: SearchPageComponent;
@@ -15,14 +15,34 @@ describe('SearchPageComponent', () => {
       imports: [
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule,
-        RouterTestingModule
+        RouterTestingModule,
       ],
-      declarations: [ SearchPageComponent ]
-    })
-    .compileComponents();
+      declarations: [SearchPageComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
+    localStorage.setItem(
+      'searchResults',
+      JSON.stringify([
+        {
+          art: 'Gesuch',
+          cubicmeter: 0,
+          date: { year: 2021, month: 5, day: 26 },
+          description: 'Suche eine Mitfahrgelegenheit. Bitte 2 Sitzplätze',
+          end: 'Berlin',
+          height: null,
+          id: 1,
+          length: null,
+          nutzer: 'VyUClws9c7d8bF9YN1zwVCuFDQz2',
+          seats: '2',
+          start: 'Köln',
+          time: { minute: 30, hour: 14, second: 0 },
+          type: 'Mitfahrgelegenheit',
+          width: null,
+        },
+      ])
+    );
     fixture = TestBed.createComponent(SearchPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
