7cfe44b397119ee7927ceef77905540c43be6d26
Patricia <patricia.ehrenreich@mni.thm.de>
2021-06-05 17:19:44 +0200

    testing

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {FormControl, FormGroup} from '@angular/forms';
+import {User} from '../model/user';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +10,7 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 
 export class RegisterComponent implements OnInit {
+  user: User[] = [];
   form = new FormGroup({
     lastName: new FormControl(),
     firstName: new FormControl(),
@@ -18,8 +20,6 @@ export class RegisterComponent implements OnInit {
     password: new FormControl(),
     passwordControl: new FormControl()
   });
-  date: any;
-
   constructor(public authService: AuthService) {
   }
 
@@ -28,15 +28,19 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     if (
-      this.form.value.password != this.form.value.passwordControl &&
-      this.form.value.lastName != '' &&
-      this.form.value.firstName != '' &&
-      this.form.value.email != '' &&
-      this.form.value.password != '' &&
-      this.form.value.passwordControl != ''
+      this.form.value.password !== this.form.value.passwordControl &&
+      this.form.value.lastName !== '' &&
+      this.form.value.firstName !== '' &&
+      this.form.value.email !== '' &&
+      this.form.value.password !== '' &&
+      this.form.value.passwordControl !== ''
     ) {
       alert('Passwörter stimmen nicht überein!');
     } else {
+      console.log(this.form.value.image);
+      const file1: File = this.form.value.image;
+      console.log("LOG");
+      console.log(file1.name);
       this.authService.register(this.form.value.lastName, this.form.value.firstName,
         this.form.value.email, this.form.value.birthday, this.form.value.image, this.form.value.password);
     }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,33 +1,74 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import firebase from 'firebase';
 import {User} from '../model/user';
-import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
+import firebase from 'firebase';
+import 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private userCollection: AngularFirestoreCollection<User>;
   users: Observable<User[]>;
   user: User | null = null;
   public userList: User[];
+  public file?: File;
 
   constructor(private afs: AngularFirestore, private auth: AngularFireAuth) {}
 
-  async register(lastName, firstName, email, birthday, image, password) {
+  async uploadProfilePicture(file: File): Promise<string> {
+    const storageRef = firebase
+      .storage()
+      .ref()
+      .child(`profile_images/${file.name}`);
+    return new Promise((resolve, reject) => {
+      storageRef.put(file).then((snapshot) => {
+        snapshot.ref
+          .getDownloadURL()
+          .then((result) => {
+            resolve(result);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    });
+  }
+
+  // tslint:disable-next-line:max-line-length
+  async register(lastName: string, firstName: string, email: string, birthday: string, image: any, password: string): Promise<any> {
+    if (image) {
+      this.uploadProfilePicture(image).then((result) => {
+        image = result;
+        this.createUser(lastName, firstName, email, birthday, image, password).catch((err) => {
+          console.error(err);
+        });
+      });
+    } else {
+      this.createUser(lastName, firstName, email, birthday, image, password).catch((err) => {
+        console.error(err);
+      });
+    }
+  }
+
+  async createUser(lastName: string, firstName: string, email: string, birthday: string, image: string, password: string): Promise<void> {
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then((response) => {
-        let registeredUser = {
+        if (image !== undefined) {
+          response.user?.updateProfile({
+            photoURL: image,
+          });
+        }
+        const registeredUser = {
           id: response.user?.uid,
-          email: email,
-          password: password,
+          email,
+          password,
           firstname: firstName,
           lastname: lastName,
-          birthday: birthday,
+          birthday,
           image: image ? image : null,
         };
         console.log(registeredUser);
