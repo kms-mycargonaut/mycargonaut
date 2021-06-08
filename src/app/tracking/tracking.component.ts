4ee8e1440d2ff0f09d01db3c5bf96884326779ba
melaniegehrke <melanie.gehrke@mni.thm.de>
2021-06-08 19:36:42 +0200

    added rating Frontend

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-tracking',
@@ -15,10 +16,18 @@ export class TrackingComponent implements OnInit {
   status3date = '02.06.2021, 18:00';
   status4date = '02.06.2021, 18:20';
   statusdate = this.status1date;
+  form = new FormGroup({
+  rating: new FormControl(),
+  title: new FormControl(),
+  ratingDescription: new FormControl()
+});
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+
+  }
 }
