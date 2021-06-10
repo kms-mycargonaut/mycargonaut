050ce79cafcc2c07b6c73fb25156d15959937b44
Bastian Jakobi <>
2021-06-10 18:22:41 +0200

    Updated model

--- src/app/model/booking.ts
+++ src/app/model/booking.ts
@@ -2,4 +2,13 @@ export class Booking {
   entry: string;
   searcher: string;
   supplier: string;
+  bookingDate: Date;
+  
+  constructor(entry: string, searcher: string, supplier: string, bookingDate: Date) {
+    entry = this.entry;
+    searcher = this.searcher;
+    supplier = this.supplier;
+    bookingDate = this.bookingDate;
+  }
+  
 }
