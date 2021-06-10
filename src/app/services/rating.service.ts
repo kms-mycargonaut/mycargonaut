1777838918a9c27e13e35eba64107b8495c85027
melaniegehrke <melanie.gehrke@mni.thm.de>
2021-06-10 17:42:41 +0200

    rating backend

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {Rating} from '../model/rating';
+import {Projects} from '@angular/cli/lib/config/schema';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +11,20 @@ export class RatingService {
 
   private ratingCollection: AngularFirestoreCollection<Rating>;
   private bookingCollection: AngularFirestoreCollection;
+  private rating;
 
   constructor(private afs: AngularFirestore) {
     this.ratingCollection = afs.collection('rating');
     this.bookingCollection = afs.collection('booking');
   }
 
-  async addRating(rating: Rating): Promise<void> {
-    // const bookingId = await this.getBookingId();
-    // rating.setBookingId(bookingId);
+  async addRating(rating: Rating, bookingId: string): Promise<void> {
+    rating.setBookingId(bookingId);
     this.ratingCollection.doc().set(Object.assign({}, rating));
   }
 
-  /*async getBookingId(): Promise<string> {
-    this.bookingCollection.doc()
-  }*/
+  getRatings(): Observable<Rating[]> {
+    return this.ratingCollection.valueChanges({idField: 'id'});
+  }
+
 }
