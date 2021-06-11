6ee801b2b8d4003f8fa54cb3650b5c1456e8d6dd
Mario <mario.kirsch94@web.de>
2021-06-11 10:16:45 +0200

    repair test-environment

--- src/app/detail-page/detail-page.component.spec.ts
+++ src/app/detail-page/detail-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailPageComponent } from './detail-page.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../environments/environment';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('DetailPageComponent', () => {
   let component: DetailPageComponent;
@@ -8,6 +12,11 @@ describe('DetailPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        RouterTestingModule,
+      ],
       declarations: [ DetailPageComponent ]
     })
     .compileComponents();
