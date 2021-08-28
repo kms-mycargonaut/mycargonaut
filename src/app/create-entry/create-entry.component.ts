aa756b6627d8c4aa7e4adf503e484f06af7cc2af
Mario Kirsch <mario.kirsch@mni.thm.de>
2021-08-28 19:12:50 +0200

    Fix test for travis ci pipeline

--- src/app/create-entry/create-entry.component.ts
+++ src/app/create-entry/create-entry.component.ts
@@ -100,7 +100,7 @@ export class CreateEntryComponent implements OnInit {
   }
 
   loadVehicles(): void {
-    if (this.authenticatedUser.uid) {
+    if (this.authenticatedUser.uid !== null) {
       this.vehicleService.getVehicles(this.authenticatedUser.uid).then(v => {
         this.vehicles = v;
       });
